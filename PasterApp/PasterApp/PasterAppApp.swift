78de1b70f00528e3b36475aa3f6f7c391fb7712b
ShinkaiTomomichi <yahharogim@gmail.com>
2022-06-25 09:39:30 +0900

    アプリを追加

---
+++ PasterApp/PasterApp/PasterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasterAppApp.swift
+//  PasterApp
+//
+//  Created by shinkaitomomichi on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PasterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
